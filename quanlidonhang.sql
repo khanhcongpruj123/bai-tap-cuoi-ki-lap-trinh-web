d10f57f0e873fc929b62b43f7ca3524b8545768e
icongkhanh <khanhcongpruj123@gmail.com>
2021-01-03 22:48:20 +0700

    chinh sua lenh sql

--- quanlidonhang.sql
+++ quanlidonhang.sql
@@ -27,7 +27,7 @@ CREATE TABLE `TBL_ACCOUNT` (
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`,`username`),
   KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;;
 
 -- ----------------------------
 -- Records of TBL_ACCOUNT
@@ -48,7 +48,7 @@ CREATE TABLE `TBL_ITEM` (
   `price` decimal(10,2) DEFAULT NULL,
   `thumnail` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;;
 
 -- ----------------------------
 -- Records of TBL_ITEM
@@ -58,6 +58,29 @@ INSERT INTO `TBL_ITEM` VALUES (1, 'Macbook Pro M1', 36000000.00, 'img_macbook_m1
 INSERT INTO `TBL_ITEM` VALUES (2, 'iPhone 12 Pro', 31000000.00, 'img_iphone_12.jpg');
 INSERT INTO `TBL_ITEM` VALUES (3, 'Macbook Air 2020', 27000000.00, 'img_mac_air_2020.jpg');
 INSERT INTO `TBL_ITEM` VALUES (4, 'Mac Mini', 18000000.00, 'img_mac_mini.png');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for TBL_USER
+-- ----------------------------
+DROP TABLE IF EXISTS `TBL_USER`;
+CREATE TABLE `TBL_USER` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `sex` int DEFAULT NULL,
+  `id_account` int NOT NULL,
+  `role` int DEFAULT NULL,
+  PRIMARY KEY (`id`,`id_account`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;;
+
+-- ----------------------------
+-- Records of TBL_USER
+-- ----------------------------
+BEGIN;
+INSERT INTO `TBL_USER` VALUES (1, 'Admin', 0, 1, 0);
+INSERT INTO `TBL_USER` VALUES (2, 'test', 0, 2, 1);
+INSERT INTO `TBL_USER` VALUES (3, 'TRANG', 1, 4, 1);
 COMMIT;
 
 -- ----------------------------
@@ -77,7 +100,7 @@ CREATE TABLE `TBL_ORDER` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `tbl_order_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `TBL_ITEM` (`id`),
   CONSTRAINT `tbl_order_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `TBL_USER` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;;
 
 -- ----------------------------
 -- Records of TBL_ORDER
@@ -88,29 +111,6 @@ INSERT INTO `TBL_ORDER` VALUES (34, 2, 2, 1, 'Nha Trang', '2020-12-21', 0);
 INSERT INTO `TBL_ORDER` VALUES (35, 1, 2, 1, 'Ki tuc xa B2', '2020-12-21', 0);
 INSERT INTO `TBL_ORDER` VALUES (37, 2, 1, 1, 'dfsdfsfsdf', '2020-11-29', 1);
 INSERT INTO `TBL_ORDER` VALUES (38, 2, 3, 1, 'Ki tuc xa B2', '2020-12-21', 0);
-COMMIT;
-
--- ----------------------------
--- Table structure for TBL_USER
--- ----------------------------
-DROP TABLE IF EXISTS `TBL_USER`;
-CREATE TABLE `TBL_USER` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `sex` int DEFAULT NULL,
-  `id_account` int NOT NULL,
-  `role` int DEFAULT NULL,
-  PRIMARY KEY (`id`,`id_account`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of TBL_USER
--- ----------------------------
-BEGIN;
-INSERT INTO `TBL_USER` VALUES (1, 'Admin', 0, 1, 0);
-INSERT INTO `TBL_USER` VALUES (2, 'test', 0, 2, 1);
-INSERT INTO `TBL_USER` VALUES (3, 'TRANG', 1, 4, 1);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
